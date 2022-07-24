136e49bbe5e90c0dda26bd8cdc6821ea429f0346
xiansheng <942972488@qq.com>
2022-07-24 18:37:53 +0800

    idea-second

--- src/main/java/com/zhao/App.java
+++ src/main/java/com/zhao/App.java
@@ -7,5 +7,6 @@ package com.zhao;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!   yeeeeeeee!" );
     }
 }
