57ad5f0774473b854bbbc30262d38c248fef2fc5
xiansheng <942972488@qq.com>
2022-07-24 19:17:04 +0800

    idea-hot-fix-first

--- src/main/java/com/zhao/App.java
+++ src/main/java/com/zhao/App.java
@@ -9,5 +9,6 @@ public class App {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!   yeeeeeeee!" );
         System.out.println( "Hello World!   yeeeeeeee!" );
+        System.out.println( "Hello hot-fix!" );
     }
 }
